da2cf2ed9e515820505c49b621c90d20117c3a71
Yanlimagimenez <yan.limma2004@gmail.com>
2022-04-13 22:33:35 -0300

    commit novo

--- app/src/main/java/com/segundods/tiptime/MainActivity.kt
+++ app/src/main/java/com/segundods/tiptime/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.segundods.tiptime
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.segundods.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
+lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val button = binding.calculateButton
+        button.setOnClickListener { calculateTip() }
+
+    }
+    fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
+        if (roundUp) {
+            val tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = formattedTip
     }
 }
